68739f0f94e11873fd04dfc448d67791714941dc
NPC-MARIMO <shivangbhaiisgreat@gmail.com>
2025-03-02 13:11:21 +0530

    some use lss changes hahahhaha

--- src/component/HomePage1.jsx
+++ src/component/HomePage1.jsx
@@ -13,7 +13,7 @@ export default function HomePage1() {
         <h1>
         The gym for high impact athletes
         </h1>
-        <h3>Fitness is not about being better than someone else. It’s about being better than you used to be</h3>
+        <h3>Fitness is not about being better than someone else. It’s about being better than you used to be.</h3>
 
         <SusBrowseButtons color="#fff"  />
         
